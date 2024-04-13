6cb32b3e57f40bb36124b95635d55d0352561f96
wendymasese <wendy.masese@student.moringaschool.com>
2024-04-13 11:51:29 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+
+    .then(response => response.json())
+
+    .then(renderBooks) 
+
+    .catch(error => console.error('Error fetching books:', error)); 
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +22,6 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
